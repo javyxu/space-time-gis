c13332343281fd9c6008359c0f46130a27bbca87
Javy <xujavy@gmail.com>
2019-04-27 22:05:37 +0800

    udapte logger

--- spacetimegis/core/__init__.py
+++ spacetimegis/core/__init__.py
@@ -6,21 +6,29 @@
 # Email: xujavy@gmail.com
 # Description: __init__.py
 '''
-from spacetimegis.config import cfg
-
 import os
-if not os.environ.get('SPARK_HOME'):
-    os.environ["SPARK_HOME"] = cfg.get('spark')['SPARK_HOME')
 
 from pyspark.sql import *
 from pkg_resources import resource_filename
 from geopyspark.geopyspark_constants import JAR
+
+from spacetimegis.config import cfg
+from spacetimegis.utils.logging_mixin import logger
+
 module_jars = [
         os.path.abspath(resource_filename('geopyspark.jars', JAR))
     ]
 jar_dirs = [(jar, os.path.dirname(jar)) for jar in module_jars]
 jars = [jar_dirs[0][0]]
 jar_string= ",".join(set(jars))
+
+spark_home = cfg.get('spark')['SPARK_HOME')
+if not spark_home:
+    logger.writedebuglog('please set SPARK_HOME in spacetimegis.cfg')
+    logger.writeinfolog('please set SPARK_HOME in spacetimegis.cfg')
+    
+if not os.environ.get('SPARK_HOME'):
+    os.environ["SPARK_HOME"] = spark_home
 
 pysc = SparkSession.builder
         .master(cfg.get('spark')['master'))
--- spacetimegis/views/download_api.py
+++ spacetimegis/views/download_api.py
@@ -12,26 +12,21 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from flask import Flask, jsonify, Response, request
+from flask import request
 
 from spacetimegis import app
-from spacetimegis.constants import LogLevel
 from spacetimegis.utils.logging_mixin import logger
 
 from spacetimegis.data.download.download_agora import * 
-
-
-def json_result(code=0, result=None, msg='success'):
-    return jsonify({"code":code, "result": result, "msg":msg})
+from .utils import json_result
 
 @app.route('/download_agora', methods=['GET'])
 def download_agora():
     try:
         path = request.args.get('savepath')
         ts, links = get_ty_links()
-        logger.writelog(LogLevel.info, (ts, links))
         download_imgs(path, ts, links)
     except Exception as e:
-        logger.writelog(LogLevel.error, e)
+        logger.writeerrorlog(e)
         return json_result(500, msg='download failure!')
     return json_result(200, msg='download sucess!')
